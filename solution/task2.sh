c2b5a543b14d0bb18092e512dfd841c7aa5615f3
Egor Savin <savin.es2007@gmail.com>
2025-09-07 17:39:03 +0700

    Update solution

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,3 +1,3 @@
-find . -name "foo.txt" -print -exec cat {} \;
-find . -name "bar.txt" -print -exec cat {} \;
-find . -name "baz.txt" -print -exec cat {} \;
+find . -name "foo.txt" -type f -print -exec cat {} \;
+find . -name "bar.txt" -type f -print -exec cat {} \;
+find . -name "baz.txt" -type f -print -exec cat {} \;
--- solution/task3.sh
+++ solution/task3.sh
@@ -1,4 +1,4 @@
-find . -name "*.txt" -exec basename {} > names \;
+find . -name "*.txt" -type f -exec basename {} > names \;
 sort names -o names
 cat names
 rm names
